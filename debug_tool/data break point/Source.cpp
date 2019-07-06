9a17f130ec324b187007ca0f1c5de78ae1ad9d8c
mono1140 <11potato30@gmail.com>
2019-07-06 22:12:25 +0900

    data break point に関するコードの追加

--- debug_tool/data break point/Source.cpp
+++ debug_tool/data break point/Source.cpp
@@ -1,1 +1,17 @@
-﻿
+﻿#include<stdio.h>
+
+int array[5] = { 0 };
+
+int main()
+{
+	for (int a = 0; a < 5; a++)
+	{
+		array[a] = a;
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d \n", array[i]);
+	}
+
+}
