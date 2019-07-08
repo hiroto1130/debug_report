d4ec8c5a195a34f75b420b6ca076e236b090f7e8
mono1140 <11potato30@gmail.com>
2019-07-08 19:09:14 +0900

    automatic variable に関するコードの追加

--- debug_tool/Automatic variable, local variable window/Source1.cpp
+++ debug_tool/Automatic variable, local variable window/Source1.cpp
@@ -1,10 +1,23 @@
 ﻿#include <stdio.h>
 
+int add(int a, int b)
+{
+	int c;
+	c = a + b;
+
+	return c;
+
+}
+
 int main()
 {
 	int HP = 100;
 	int MP = 50;
 	const char *name = "mono";
+	int answer;
+	answer = add(5, 7);
+
 
 	printf(" 体力：%2d \n 魔法力：%2d \n 名前：%2s \n", HP, MP, name);
+	printf("足し算結果：%d \n", answer);
 }
